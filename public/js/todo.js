5df2f6b5cb24d75302565aa9eb38d00de7e6b152
Vladf29 <boxforemailand@gmail.com>
2018-04-25 11:15:18 +0300

    delete/done

--- public/js/todo.js
+++ public/js/todo.js
@@ -3,57 +3,85 @@
 downloadTasks();
 
 const form = document.querySelector('.js-form');
+const list = document.querySelector('.js-list');
 
-form.addEventListener('submit', handlerForm);
+form.addEventListener('submit', handlerFormAddTask);
+list.addEventListener('click', handlerList);
 
-const checkType = (type) => {
+function checkType(type) {
     return Object.prototype.toString.call(type).slice(8, -1);
 }
 
-const addTaskToDB = (task) => {
+function httpRequest(url = '', method = 'GET', header = [], data) {
     return new Promise((resolve, reject) => {
+        if (!url) reject(new Error('without url'));
+
         const xhr = new XMLHttpRequest();
-        xhr.open('POST', '/todo/add');
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify(task));
+        xhr.open(method, url);
+        if (header.length === 2) xhr.setRequestHeader(header[0], header[1]);
+        data ? xhr.send(data) : xhr.send();
 
         xhr.onload = function () {
-            if (this.status === 200) resolve(JSON.parse(this.response));
-            else reject(this.response);
+            if (this.status === 200) {
+                resolve(this.response);
+            } else {
+                reject(this.response);
+            }
         }
 
         xhr.onerror = function () {
-            reject(new Error('no connection'));
+            reject(new Error('Something went wrong'));
         }
     });
 }
 
-function downloadTasks() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', '/todo/all');
-    xhr.send();
-
-    xhr.onload = function () {
-        if (this.status === 200) {
-            addTaskToList(JSON.parse(this.response));
-        } else {
-            console.log(this.response);
-        }
+async function downloadTasks() {
+    try {
+        const result = await httpRequest('/todo/all');
+        addTaskToList(JSON.parse(result));
+    } catch (err) {
+        console.log(err);
     }
 }
 
-async function handlerForm(event) {
+async function handlerFormAddTask(event) {
     event.preventDefault();
 
     const val = this.elements['taskField'].value;
     if (!val) return;
 
     try {
-        const result = await addTaskToDB({
+        const data = JSON.stringify({
             task: val,
             done: false
         });
-        addTaskToList(result);
+        await httpRequest('/todo/add', 'POST', ['Content-Type', 'application/json'], data);
+        downloadTasks();
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+async function handlerList(event) {
+    event.preventDefault();
+    const target = event.target;
+    if (!target.hasAttribute('data-action')) return;
+
+    const action = target.getAttribute('data-action');
+    const li = target.closest('.js-item-task');
+    const id = li.getAttribute('data-id-task');
+
+    try {
+        const data = JSON.stringify({
+            id
+        });
+        if (action === 'delete') {
+            const result = await httpRequest('/todo/delete', 'DELETE', ['Content-Type', 'application/json'], data);
+            this.removeChild(li);
+        } else if (action === 'done') {
+            const result = await httpRequest('/todo/done', 'PUT', ['Content-Type', 'application/json'], data);
+            downloadTasks();
+        }
     } catch (err) {
         console.log(err.message);
     }
@@ -63,16 +91,22 @@ function addTaskToList(task = {}) {
     if (!task) return;
 
     const ul = document.querySelector('.js-list');
-
-    if (checkType(task) === 'Object') {
-        const li = document.createElement('li');
-        li.textContent = task.task;
-        ul.appendChild(li);
-    } else if (checkType(task) === 'Array') {
+    ul.innerHTML = '';
+    const tempBtns = `
+        <div class='btn-group btn-group-sm'>
+            <button class='btn btn-secondary' data-action='done'>Done</button>
+            <button class='btn btn-secondary' data-action='delete'>Delete</button>
+            <button class='btn btn-secondary' data-action='edit'>Edit</button>
+        </div>
+    `
+    if (checkType(task) === 'Array') {
         task.forEach((item, ind) => {
             const li = document.createElement('li');
-            li.classList.add('list-group-item', 'list-group-item-action', 'd-flex', 'justify-content-between', 'align-items-center');
-            li.innerHTML = `${item.task} <span class="badge badge-secondary badge-pill">${ind + 1}</span>`
+            const clses = ['list-group-item', 'list-group-item-action', 'd-flex', 'justify-content-between', 'align-items-center', 'js-item-task'];
+            li.classList.add(...clses);
+            li.setAttribute('data-id-task', item._id);
+            li.setAttribute('data-done', item.done);
+            li.innerHTML = `<span class='js-task'>${item.task}</span>${tempBtns}`
             ul.appendChild(li);
         });
     }
